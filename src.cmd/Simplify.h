c94241555ecf576ae1e07d6104a00396ec48fbcb
donaldnguyen99 <donaldnguyen99@gmail.com>
2019-03-26 15:28:32 -0700

    Comment on unused line

--- src.cmd/Simplify.h
+++ src.cmd/Simplify.h
@@ -452,7 +452,7 @@ namespace Simplify
 		// main iteration loop
 		int deleted_triangles=0;
 		std::vector<int> deleted0,deleted1;
-		//int triangle_count=triangles.size();
+		//int triangle_count=triangles.size(); // unused variable in function
 		//int iteration = 0;
 		//loop(iteration,0,100)
 		for (int iteration = 0; iteration < 9999; iteration ++)
